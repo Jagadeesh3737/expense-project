1e97e5249e10858dddf75e6e8662850b55721827
jagadeesh3737 <jagadeeshvarma444@gmail.com>
2023-11-10 14:50:23 +0530

    frontend pushing

--- frontend.sh
+++ frontend.sh
@@ -8,11 +8,26 @@ if [ $? -eq 0 ]; then
 fi
 }
 
-
 echo -e "${color} installing nginx \e[0m"
-dnf install nginx -y
+dnf install nginx -y &>>$log_file
 status_check
 
+echo -e "${color} enabling nginx \e[0m"
+systemctl enable nginx
+status_check
 
+echo -e "${color} starting nginx \e[0m"
+systemctl start nginx &>>$log_file
+status_check
 
+echo -e "${color} removing the default content of nginx \e[0m"
+rm -rf /usr/share/nginx/html/* &>>$log_file
+status_check
 
+echo -e "${color} copying expense.conf file \e[0m"
+cp /etc/nginx/default.d/expense.conf &>>$log_file
+status_check
+
+echo -e "${color} restarting nginx \e[0m"
+systemctl restart nginx
+status_check
