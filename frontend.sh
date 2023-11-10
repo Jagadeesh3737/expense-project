f767f43ca9d242aa8ed693b58791f1cc09b42cc1
jagadeesh3737 <jagadeeshvarma444@gmail.com>
2023-11-10 14:29:57 +0530

    frontend pushing

---
+++ frontend.sh
@@ -0,0 +1,17 @@
+color="\e[32m"
+log_file=/tmp/expense.log
+status_check() {
+  if { $? -eq 0 }; then
+      echo -e "\e[34m success \e[0m"
+    else
+      echo -e "\e[31m failed \e[0m"
+  fi
+}
+
+echo -e "${color} installing nginx \e[0m"
+dnf install nginx -y &>>$log_log_file
+status_check
+
+
+
+
