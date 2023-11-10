435777e14417cd653c7d4b50a20d787007ad485f
jagadeesh3737 <jagadeeshvarma444@gmail.com>
2023-11-10 17:38:16 +0530

    frontend pushing

--- backend.sh
+++ backend.sh
@@ -46,7 +46,7 @@ echo -e "${color} npm installing \e[0m"
 npm install &>>$log_file
 status_check
 
-if[ ! -f backend.zip ]; then
+if [ ! -f backend.zip ]; then
   echo -e "${color} downloading the backend application code \e[0m"
   curl -o /tmp/backend.zip https://expense-artifacts.s3.amazonaws.com/backend.zip &>>$log_file
 fi
