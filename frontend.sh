53165922b1c1efe661dce13e3b43d79689a0cc5d
jagadeesh3737 <jagadeeshvarma444@gmail.com>
2023-11-10 15:04:29 +0530

    frontend pushing

--- frontend.sh
+++ frontend.sh
@@ -12,14 +12,6 @@ echo -e "${color} installing nginx \e[0m"
 dnf install nginx -y &>>$log_file
 status_check
 
-echo -e "${color} enabling nginx \e[0m"
-systemctl enable nginx &>>$log_file
-status_check
-
-echo -e "${color} starting nginx \e[0m"
-systemctl start nginx &>>$log_file
-status_check
-
 echo -e "${color} copying expense.conf file \e[0m"
 cp /etc/nginx/default.d/expense.conf &>>$log_file
 status_check
@@ -29,12 +21,21 @@ rm -rf /usr/share/nginx/html/* &>>$log_file
 status_check
 
 echo -e "${color} downloading the frontend content \e[0m"
-curl -o /tmp/frontend.zip /tmp/frontend.zip https://expense-artifacts.s3.amazonaws.com/frontend.zip
+curl -o /tmp/frontend.zip https://expense-artifacts.s3.amazonaws.com/frontend.zip
 status_check
 
 echo -e "${color} unzipping frontend content \e[0m"
 cd /usr/share/nginx/html
 unzip /tmp/frontend.zip &>>$log
+
+echo -e "${color} enabling nginx \e[0m"
+systemctl enable nginx &>>$log_file
+status_check
+
+echo -e "${color} starting nginx \e[0m"
+systemctl start nginx &>>$log_file
+status_check
+
 
 echo -e "${color} restarting nginx \e[0m"
 systemctl restart nginx
