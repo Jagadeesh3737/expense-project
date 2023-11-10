b51db27eec12dbd6ed437be6a3488bc4fe428d0e
jagadeesh3737 <jagadeeshvarma444@gmail.com>
2023-11-10 14:33:25 +0530

    frontend pushing

--- frontend.sh
+++ frontend.sh
@@ -1,7 +1,7 @@
 color="\e[32m"
 log_file=/tmp/expense.log
 status_check() {
-if { $? -eq 0 }; then
+if [ $? -eq 0 ]; then
       echo -e "\e[34m success \e[0m"
     else
       echo -e "\e[31m failed \e[0m"
